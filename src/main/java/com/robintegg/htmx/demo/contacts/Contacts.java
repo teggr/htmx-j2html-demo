da31ca97042255b61476eec25d8b37ed88bf67d0
teggr <robin@tegg.me.uk>
2023-08-03 21:22:27 +0100

    add hx-boost

--- src/main/java/com/robintegg/htmx/demo/contacts/Contacts.java
+++ src/main/java/com/robintegg/htmx/demo/contacts/Contacts.java
@@ -9,41 +9,46 @@ import java.util.List;
 @Component
 public class Contacts {
 
-    private List<Contact> contactList = new ArrayList<>();
+  private List<Contact> contactList = new ArrayList<>();
 
-    private int idSequence = 4;
+  private int idSequence = 4;
 
-    @PostConstruct
-    void loadContacts() {
-        contactList.add(new Contact(1, "John", "Smith", "123-456-7890", "john@example.comz"));
-        contactList.add(new Contact(2, "Dana", "Crandith", "123-456-7890", "dcran@example.com"));
-        contactList.add(new Contact(3, "Edith", "Neutvaar", "123-456-7890", "an@example.com"));
-    }
+  @PostConstruct
+  void loadContacts() {
+    contactList.add(new Contact(1, "John", "Smith", "123-456-7890", "john@example.comz"));
+    contactList.add(new Contact(2, "Dana", "Crandith", "123-456-7890", "dcran@example.com"));
+    contactList.add(new Contact(3, "Edith", "Neutvaar", "123-456-7890", "an@example.com"));
+  }
 
-    public List<Contact> all() {
-        return new ArrayList<>(contactList);
-    }
+  public List<Contact> all() {
+    return new ArrayList<>(contactList);
+  }
 
-    public List<Contact> search(String q) {
-        return contactList.stream().toList();
-    }
+  public List<Contact> search(String q) {
+    return contactList.stream()
+        .filter(c -> c.email().toLowerCase().contains(q.toLowerCase()) ||
+            c.phone().toLowerCase().contains(q.toLowerCase()) ||
+            c.first().toLowerCase().contains(q.toLowerCase()) ||
+            c.last().toLowerCase().contains(q.toLowerCase()))
+        .toList();
+  }
 
-    public void add(Contact c) {
-        contactList.add(new Contact(++idSequence, c.first(), c.last(), c.phone(), c.email()));
-    }
+  public void add(Contact c) {
+    contactList.add(new Contact(++idSequence, c.first(), c.last(), c.phone(), c.email()));
+  }
 
-    public Contact find(Integer id) {
-        return contactList.stream().filter(c -> c.id().equals(id)).findFirst().orElseThrow();
-    }
+  public Contact find(Integer id) {
+    return contactList.stream().filter(c -> c.id().equals(id)).findFirst().orElseThrow();
+  }
 
-    public void update(Integer id, Contact c) {
-        Contact contact = find(id);
-        contactList.remove(contact);
-        contactList.add(new Contact(id, c.first(), c.last(), c.phone(), c.email()));
-    }
+  public void update(Integer id, Contact c) {
+    Contact contact = find(id);
+    contactList.remove(contact);
+    contactList.add(new Contact(id, c.first(), c.last(), c.phone(), c.email()));
+  }
 
-    public void delete(Integer id) {
-        Contact contact = find(id);
-        contactList.remove(contact);
-    }
+  public void delete(Integer id) {
+    Contact contact = find(id);
+    contactList.remove(contact);
+  }
 }
--- src/main/java/com/robintegg/htmx/demo/web/pages/Layout.java
+++ src/main/java/com/robintegg/htmx/demo/web/pages/Layout.java
@@ -9,42 +9,43 @@ import static j2html.TagCreator.*;
 
 public class Layout {
 
-    public static DomContent withContent(Map<String, ?> model, HttpServletRequest request, DomContent... content) {
-        return new Layout(model, request, content).getLayout();
-    }
+  public static DomContent withContent(Map<String, ?> model, HttpServletRequest request, DomContent... content) {
+    return new Layout(model, request, content).getLayout();
+  }
 
-    private final Map<String, ?> model;
-    private final HttpServletRequest request;
-    private final DomContent[] content;
+  private final Map<String, ?> model;
+  private final HttpServletRequest request;
+  private final DomContent[] content;
 
-    public Layout(Map<String, ?> model, HttpServletRequest request, DomContent[] content) {
-        this.model = model;
-        this.request = request;
-        this.content = content;
-    }
+  public Layout(Map<String, ?> model, HttpServletRequest request, DomContent[] content) {
+    this.model = model;
+    this.request = request;
+    this.content = content;
+  }
 
-    private DomContent getLayout() {
+  private DomContent getLayout() {
 
-        String msg = (String) model.get("msg");
+    String msg = (String) model.get("msg");
 
-        return join(document(), html(
-                head()
-                        .withTitle("Contacts App")
-                        .with(
-                                link().withRel("stylesheet").withHref("https://unpkg.com/missing.css@1.0.9/dist/missing.min.css")
-                        ),
-                body(
-                        h1("Contacts.app"),
-                        h2("A Demo Contacts Application"),
-                        hr(),
-                        iff(msg != null,
-                                div((String) msg).withClass("box")
-                        ),
-                        div().withClass("container").with(
-                                content
-                        )
-                )
-        ));
-    }
+    return join(document(), html(
+        head()
+            .withTitle("Contacts App")
+            .with(
+                link().withRel("stylesheet").withHref("https://unpkg.com/missing.css@1.0.9/dist/missing.min.css"),
+                script().withSrc("https://unpkg.com/htmx.org")
+            ),
+        body().attr("hx-boost", true).with(
+            h1("Contacts.app"),
+            h2("A Demo Contacts Application"),
+            hr(),
+            iff(msg != null,
+                div((String) msg).withClass("box")
+            ),
+            div().withClass("container").with(
+                content
+            )
+        )
+    ));
+  }
 
 }
